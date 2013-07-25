f2ac3b3ef627a850e4d1e8069f612663762aa5e6
Alexander.Trofimov <Alexander.Trofimov@onlyoffice.com>
2013-07-25 09:49:52 +0000

    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/nodeJSProjects@48249 954022d7-b5bf-4e40-9824-e11837661b57

--- InstallAndRunCoAuthoring.bat
+++ InstallAndRunCoAuthoring.bat
@@ -2,22 +2,24 @@
 
 SET MONGO_DB_SERVICE_NAME=ASC Mongo DB Server
 SET CO_AUTHORING_SERVICE_NAME=ASC CoAuthoring Server
-SET INSTAL_SCRIPT_FOLDER=install_script
-SET DB_FILE_PATH=data\db
+SET INSTAL_SERVICE_FOLDER=Common
+SET INSTAL_SCRIPT_FOLDER=install
+SET SERVICE_FOLDER=CoAuthoring
+SET DB_FILE_PATH=CoAuthoring\data\db
 
 IF NOT EXIST %~dp0%DB_FILE_PATH% MKDIR %~dp0%DB_FILE_PATH%
 @IF NOT "%ERRORLEVEL%"=="0" goto ERROR
 
-call %~dp0\%INSTAL_SCRIPT_FOLDER%\InstallAndRunService.bat "%MONGO_DB_SERVICE_NAME%" "%~dp0mongodb\bin\mongod.exe --journal --dbpath "%~dp0%DB_FILE_PATH%"
+call %~dp0\%INSTAL_SERVICE_FOLDER%\%INSTAL_SCRIPT_FOLDER%\InstallAndRunService.bat "%MONGO_DB_SERVICE_NAME%" "%~dp0\%SERVICE_FOLDER%\mongodb\bin\mongod.exe --journal --dbpath "%~dp0%DB_FILE_PATH%"
 @IF NOT "%ERRORLEVEL%"=="0" goto ERROR
 
-call %~dp0\%INSTAL_SCRIPT_FOLDER%\ConfigMongoDB.bat
+call %~dp0\%SERVICE_FOLDER%\%INSTAL_SCRIPT_FOLDER%\ConfigMongoDB.bat
 @IF NOT "%ERRORLEVEL%"=="0" goto ERROR
 
-call %~dp0\%INSTAL_SCRIPT_FOLDER%\InstallNodeJSModules.bat
+call %~dp0\%SERVICE_FOLDER%\%INSTAL_SCRIPT_FOLDER%\InstallNodeJSModules.bat
 @IF NOT "%ERRORLEVEL%"=="0" goto ERROR
 
-call %~dp0\%INSTAL_SCRIPT_FOLDER%\InstallAndRunService.bat "%CO_AUTHORING_SERVICE_NAME%" "node.exe %~dp0sources\server.js"
+call %~dp0\%INSTAL_SERVICE_FOLDER%\%INSTAL_SCRIPT_FOLDER%\InstallAndRunService.bat "%CO_AUTHORING_SERVICE_NAME%" "node.exe %~dp0\%SERVICE_FOLDER%\sources\server.js"
 @IF NOT "%ERRORLEVEL%"=="0" goto ERROR
 
 
