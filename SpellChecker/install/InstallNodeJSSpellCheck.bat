e1538d7ae4cc576a058062ac7b288dbc41839361
Alexander.Trofimov <Alexander.Trofimov@onlyoffice.com>
2013-07-25 09:30:09 +0000

    update install spellCheck
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/nodeJSProjects@48232 954022d7-b5bf-4e40-9824-e11837661b57

--- SpellChecker/install/InstallNodeJSSpellCheck.bat
+++ SpellChecker/install/InstallNodeJSSpellCheck.bat
@@ -11,17 +11,19 @@ ECHO ----------------------------------------
 
 call npm install express@2.5.8 || exit /b 1
 call npm install sockjs || exit /b 1
-call npm install log4js || exit /b 1
 
 call npm install -g node-gyp || exit /b 1
 call npm install nodehun@0.0.5 || exit /b 1
 
-COPY nodehun\nodehun.cpp node_modules\nodehun\src\nodehun.cpp
-COPY nodehun\replist.hxx node_modules\nodehun\src\hunspell\src\hunspell\replist.hxx
+COPY sources\nodehun\nodehun.cpp node_modules\nodehun\src\nodehun.cpp
+COPY sources\nodehun\replist.hxx node_modules\nodehun\src\hunspell\src\hunspell\replist.hxx
 
 cd /D node_modules\nodehun\src || exit /b 1
 call node-gyp configure || exit /b 1
 call node-gyp build	|| exit /b 1
+
+cd /D ..\..\..\..\Common || exit /b 1
+call npm install log4js || exit /b 1
 
 CD /D %RUN_FOLDER% || exit /b 1
 
