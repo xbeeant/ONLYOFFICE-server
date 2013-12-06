cb73f6d6d04251f8d80e2f330e860394c1d155e7
Alexander.Trofimov <Alexander.Trofimov@onlyoffice.com>
2013-12-06 14:58:20 +0000

    refactoring
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/nodeJSProjects@52939 954022d7-b5bf-4e40-9824-e11837661b57

--- CoAuthoring/sources/server.js
+++ CoAuthoring/sources/server.js
@@ -1,43 +1,35 @@
 ﻿var config = require('./config.json');
-
 process.env.NODE_ENV = config['server']['mode'];
 
-var logger = require('./../../Common/sources/logger');
-
-// 2. Express server
-var express = require('express');
-
-var http = require('http');
-var https = require('https');
-
-var fs = require("fs");
-	
-var app = express();
-var server = {};
+var logger = require('./../../Common/sources/logger'),
+	express = require('express'),
+	http = require('http'),
+	https = require('https'),
+	fs = require("fs"),
+	docsCoServer = require('./DocsCoServer'),
+	app = express(),
+	server = null;
 
 if (config['ssl']) {
-	var privateKey = fs.readFileSync(config['ssl']['key']).toString();
-	var certificate = fs.readFileSync(config['ssl']['cert']).toString();
-	
-	var options = {key: privateKey, cert:certificate};
-	
+	var privateKey = fs.readFileSync(config['ssl']['key']).toString(),
+		certificate = fs.readFileSync(config['ssl']['cert']).toString(),
+		options = {key: privateKey, cert:certificate};
+
 	server = https.createServer(options, app);
 } else {
 	server = http.createServer(app);
 }
 
-app.configure('development', function () {
+app.configure('development', function() {
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
-app.configure('production', function(){
+app.configure('production', function() {
     app.use(express.errorHandler());
 });
 
-var docsCoServer  = require('./DocsCoServer');
-
-docsCoServer.install(server, function(){
-	server.listen(config['server']['port'], function(){
+docsCoServer.install(server, function() {
+	server.listen(config['server']['port'], function() {
 		logger.info("Express server listening on port %d in %s mode", config['server']['port'], app.settings.env);
 	});
 	
