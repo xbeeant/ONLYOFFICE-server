f65fad03f26ffac6974e3c9a1f1f0dc7a9c49a2f
Alexey.Golubev <Alexey.Golubev@onlyoffice.com>
2014-01-31 14:36:32 +0000

    Отдельно вынесен билд из инсталяции компонентов nodejs. Т.к. планируется хранить node_modules gjl svn.
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/nodeJSProjects@54066 954022d7-b5bf-4e40-9824-e11837661b57

--- CoAuthoring/install/InstallNodeJSModules.bat
+++ CoAuthoring/install/InstallNodeJSModules.bat
@@ -15,7 +15,7 @@ call npm install sockjs --production|| exit /b 1
 call npm install mongodb@1.1.4 --production || exit /b 1
 
 cd /D ..\Common || exit /b 1
-call npm install log4js --production || exit /b 1
+call npm install log4js@0.6.2 --production || exit /b 1
 
 CD /D %RUN_FOLDER% || exit /b 1
 
