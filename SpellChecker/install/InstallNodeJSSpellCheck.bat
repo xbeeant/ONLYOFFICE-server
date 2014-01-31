b48cbe729ced78f9e417ac564eb694e23e2ffa76
Alexey.Golubev <Alexey.Golubev@onlyoffice.com>
2014-01-31 14:36:32 +0000

    Отдельно вынесен билд из инсталяции компонентов nodejs. Т.к. планируется хранить node_modules gjl svn.
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/nodeJSProjects@54066 954022d7-b5bf-4e40-9824-e11837661b57

--- SpellChecker/install/InstallNodeJSSpellCheck.bat
+++ SpellChecker/install/InstallNodeJSSpellCheck.bat
@@ -12,16 +12,8 @@ ECHO ----------------------------------------
 call npm install express --production || exit /b 1
 call npm install sockjs --production || exit /b 1
 
-call npm install -g node-gyp || exit /b 1
-
-XCOPY /S nodehun node_modules\nodehun\ /Y 
-
-cd /D node_modules\nodehun\src || exit /b 1
-call node-gyp configure || exit /b 1
-call node-gyp build	|| exit /b 1
-
-cd /D ..\..\..\..\Common || exit /b 1
-call npm install log4js@0.6.2 --production --no-optional || exit /b 1
+cd /D ..\Common || exit /b 1
+call npm install log4js@0.6.2 --production || exit /b 1
 
 CD /D %RUN_FOLDER% || exit /b 1
 
