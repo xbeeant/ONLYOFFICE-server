4f512223a751189cb40a220a4bea8d9f8cd6caff
Alexander.Trofimov <Alexander.Trofimov@onlyoffice.com>
2015-04-01 15:08:34 +0000

    Добавил package.json для установки spellChecker. Обновил модули для работы.
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/nodeJSProjects@61785 954022d7-b5bf-4e40-9824-e11837661b57

--- SpellChecker/install/InstallNodeJSSpellCheck.bat
+++ SpellChecker/install/InstallNodeJSSpellCheck.bat
@@ -2,18 +2,16 @@ ECHO OFF
 
 SET RUN_FOLDER=%CD%
 
-CD /D %~dp0..\ || exit /b 1
-
 ECHO.
 ECHO ----------------------------------------
 ECHO Install node.js module spellCheck (nodehun) 
 ECHO ----------------------------------------
 
-call npm install express --production || exit /b 1
-call npm install sockjs --production || exit /b 1
+CD /D %~dp0..\ || exit /b 1
+call npm install
 
 cd /D ..\Common || exit /b 1
-call npm install log4js@0.6.2 --production || exit /b 1
+call npm install
 
 CD /D %RUN_FOLDER% || exit /b 1
 
