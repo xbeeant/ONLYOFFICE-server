0264edd2a8c296f1c18c59cde7f8686171554604
Alexey.Golubev <Alexey.Golubev@onlyoffice.com>
2015-09-01 13:01:17 +0000

    Обновлен скрипт для инсталляции модулей nodejs, изменено его расположение. Удалены не используемые скрипты.
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/nodeJSProjects@64434 954022d7-b5bf-4e40-9824-e11837661b57

---
+++ InstallNodeJSModules.bat
@@ -0,0 +1,27 @@
+ECHO OFF
+
+ECHO.
+ECHO ----------------------------------------
+ECHO Install node.js modules 
+ECHO ----------------------------------------
+
+CD /D %~dp0\CoAuthoring || goto ERROR
+call npm install
+
+cd /D ..\Common || goto ERROR
+call npm install
+
+cd /D ..\FileConverter || goto ERROR
+call npm install
+
+cd /D ..\FileStorage || goto ERROR
+call npm install
+
+cd /D ..\SpellChecker || goto ERROR
+call npm install
+
+:ERROR
+:SUCCESS
+pause
+
+exit /b 0
