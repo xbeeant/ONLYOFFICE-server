43fa1332fb7d29715435d4fb96d4ef698df4fc0b
Alexander.Trofimov <Alexander.Trofimov@onlyoffice.com>
2015-09-10 13:31:32 +0000

    update nodejs to 4.0.0 При запуске nodejs делаем еще и установку модулей (на случай, если они обновятся).
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/nodeJSProjects@64623 954022d7-b5bf-4e40-9824-e11837661b57

--- run_services.bat
+++ run_services.bat
@@ -1,13 +1,16 @@
 SET RUN_DIR=%CD%
 
+call %RUN_DIR%\install_npm_modules.bat
+@IF NOT "%ERRORLEVEL%"=="0" goto ERROR
+
 SET NODE_ENV=development-windows
 SET NODE_CONFIG_DIR=%RUN_DIR%\Common\config
 
 cd %RUN_DIR%\CoAuthoring\sources
-start node --harmony server.js
+start node server.js
 
 cd %RUN_DIR%\FileConverter\sources
-start node --harmony convertermaster.js
+start node convertermaster.js
 
 cd %RUN_DIR%\FileStorage\sources
 start node server.js
@@ -15,4 +18,6 @@ start node server.js
 cd %RUN_DIR%\SpellChecker\sources
 start node server.js
 
+:ERROR
+:SUCCESS
 pause
