91a7c2c61b8f30d01faec08f796a47b59e39b76c
Sergey.Konovalov <Sergey.Konovalov@onlyoffice.com>
2015-09-14 11:36:36 +0000

    rabbitMQ reconnect
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/nodeJSProjects@64696 954022d7-b5bf-4e40-9824-e11837661b57

--- CoAuthoring/sources/pubsubRabbitMQ.js
+++ CoAuthoring/sources/pubsubRabbitMQ.js
@@ -6,16 +6,14 @@ var rabbitMQCore = require('./../../Common/sources/rabbitMQCore');
 
 var cfgRabbitExchangePubSub = require('config').get('rabbitmq.exchangepubsub');
 
-function PubsubRabbitMQ() {
-  this.channelPublish = null;
-  this.exchangePublish = null;
-}
-util.inherits(PubsubRabbitMQ, events.EventEmitter);
-PubsubRabbitMQ.prototype.init = function(callback) {
-  var pubsub = this;
+function init(pubsub, callback) {
   utils.spawn(function* () {
+    var e = null;
     try {
-      var conn = yield rabbitMQCore.connetPromise();
+      var conn = yield rabbitMQCore.connetPromise(function () {
+        clear(pubsub);
+        init(pubsub, null);
+      });
       pubsub.channelPublish = yield rabbitMQCore.createChannelPromise(conn);
       pubsub.exchangePublish = yield rabbitMQCore.assertExchangePromise(pubsub.channelPublish, cfgRabbitExchangePubSub,
         'fanout', {durable: true});
@@ -23,20 +21,52 @@ PubsubRabbitMQ.prototype.init = function(callback) {
       var channelReceive = yield rabbitMQCore.createChannelPromise(conn);
       var queue = yield rabbitMQCore.assertQueuePromise(channelReceive, '', {autoDelete: true, exclusive: true});
       channelReceive.bindQueue(queue, cfgRabbitExchangePubSub, '');
-      yield rabbitMQCore.consumePromise(channelReceive, queue, function(message) {
+      yield rabbitMQCore.consumePromise(channelReceive, queue, function (message) {
         if (message) {
           pubsub.emit('message', message.content.toString());
         }
         channelReceive.ack(message);
       }, {noAck: false});
-      callback(null);
+      //process messages received while reconnection time
+      repeat(pubsub);
     } catch (err) {
-      callback(err);
+      e = err;
+    }
+    if (callback) {
+      callback(e);
     }
   });
+}
+function clear(pubsub) {
+  pubsub.channelPublish = null;
+  pubsub.exchangePublish = null;
+}
+function repeat(pubsub) {
+  for (var i = 0; i < pubsub.publishStore.length; ++i) {
+    publish(pubsub, pubsub.publishStore[i]);
+  }
+  pubsub.publishStore.length = 0;
+}
+function publish(pubsub, data) {
+  pubsub.channelPublish.publish(pubsub.exchangePublish, '', data);
+}
+
+function PubsubRabbitMQ() {
+  this.channelPublish = null;
+  this.exchangePublish = null;
+  this.publishStore = [];
+}
+util.inherits(PubsubRabbitMQ, events.EventEmitter);
+PubsubRabbitMQ.prototype.init = function (callback) {
+  init(this, callback);
 };
-PubsubRabbitMQ.prototype.publish = function(data) {
-  this.channelPublish.publish(this.exchangePublish, '', new Buffer(data));
+PubsubRabbitMQ.prototype.publish = function (message) {
+  var data = new Buffer(message);
+  if (null != this.channelPublish) {
+    publish(this, data);
+  } else {
+    this.publishStore.push(data);
+  }
 };
 
 module.exports = PubsubRabbitMQ;
