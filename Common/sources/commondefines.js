9e43b6907203e6de8bb64f7655e4e300c4649c9c
Sergey.Konovalov <Sergey.Konovalov@onlyoffice.com>
2015-10-27 12:58:01 +0000

    значения по умолчанию для codepage, delimiter в ConverterService
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/nodeJSProjects@65476 954022d7-b5bf-4e40-9824-e11837661b57

--- Common/sources/commondefines.js
+++ Common/sources/commondefines.js
@@ -388,7 +388,94 @@ function ErrorWithResult() {
   this.data = null;
 }
 
+var c_oAscCsvDelimiter = {
+  None: 0,
+  Tab: 1,
+  Semicolon: 2,
+  Сolon: 3,
+  Comma: 4,
+  Space: 5
+};
+var c_oAscEncodings = [
+  [ 0,    28596, "ISO-8859-6",       "Arabic (ISO 8859-6)" ],
+  [ 1,    720,   "DOS-720",          "Arabic (OEM 720)" ],
+  [ 2,    1256,  "windows-1256",     "Arabic (Windows)" ],
+
+  [ 3,    28594, "ISO-8859-4",       "Baltic (ISO 8859-4)" ],
+  [ 4,    28603, "ISO-8859-13",      "Baltic (ISO 8859-13)" ],
+  [ 5,    775,   "IBM775",           "Baltic (OEM 775)" ],
+  [ 6,    1257,  "windows-1257",     "Baltic (Windows)" ],
+
+  [ 7,    28604, "ISO-8859-14",      "Celtic (ISO 8859-14)" ],
+
+  [ 8,    28595, "ISO-8859-5",       "Cyrillic (ISO 8859-5)" ],
+  [ 9,    20866, "KOI8-R",           "Cyrillic (KOI8-R)" ],
+  [ 10,   21866, "KOI8-U",           "Cyrillic (KOI8-U)" ],
+  [ 11,   10007, "x-mac-cyrillic",   "Cyrillic (Mac)" ],
+  [ 12,   855,   "IBM855",           "Cyrillic (OEM 855)" ],
+  [ 13,   866,   "cp866",            "Cyrillic (OEM 866)" ],
+  [ 14,   1251,  "windows-1251",     "Cyrillic (Windows)" ],
+
+  [ 15,   852,   "IBM852",           "Central European (OEM 852)" ],
+  [ 16,   1250,  "windows-1250",     "Central European (Windows)" ],
+
+  [ 17,   950,   "Big5",             "Chinese (Big5 Traditional)" ],
+  [ 18,   936,   "GB2312",           "Central (GB2312 Simplified)" ],
+
+  [ 19,   28592, "ISO-8859-2",       "Eastern European (ISO 8859-2)" ],
+
+  [ 20,   28597, "ISO-8859-7",       "Greek (ISO 8859-7)" ],
+  [ 21,   737,   "IBM737",           "Greek (OEM 737)" ],
+  [ 22,   869,   "IBM869",           "Greek (OEM 869)" ],
+  [ 23,   1253,  "windows-1253",     "Greek (Windows)" ],
+
+  [ 24,   28598, "ISO-8859-8",       "Hebrew (ISO 8859-8)" ],
+  [ 25,   862,   "DOS-862",          "Hebrew (OEM 862)" ],
+  [ 26,   1255,  "windows-1255",     "Hebrew (Windows)" ],
+
+  [ 27,   932,   "Shift_JIS",        "Japanese (Shift-JIS)" ],
+
+  [ 28,   949,   "KS_C_5601-1987",   "Korean (Windows)" ],
+  [ 29,   51949, "EUC-KR",           "Korean (EUC)" ],
+
+  [ 30,   861,   "IBM861",           "North European (Icelandic OEM 861)" ],
+  [ 31,   865,   "IBM865",           "North European (Nordic OEM 865)" ],
+
+  [ 32,   874,   "windows-874",      "Thai (TIS-620)" ],
+
+  [ 33,   28593, "ISO-8859-3",       "Turkish (ISO 8859-3)" ],
+  [ 34,   28599, "ISO-8859-9",       "Turkish (ISO 8859-9)" ],
+  [ 35,   857,   "IBM857",           "Turkish (OEM 857)" ],
+  [ 36,   1254,  "windows-1254",     "Turkish (Windows)" ],
+
+  [ 37,   28591, "ISO-8859-1",       "Western European (ISO-8859-1)" ],
+  [ 38,   28605, "ISO-8859-15",      "Western European (ISO-8859-15)" ],
+  [ 39,   850,   "IBM850",           "Western European (OEM 850)" ],
+  [ 40,   858,   "IBM858",           "Western European (OEM 858)" ],
+  [ 41,   860,   "IBM860",           "Western European (OEM 860 : Portuguese)" ],
+  [ 42,   863,   "IBM863",           "Western European (OEM 863 : French)" ],
+  [ 43,   437,   "IBM437",           "Western European (OEM-US)" ],
+  [ 44,   1252,  "windows-1252",     "Western European (Windows)" ],
+
+  [ 45,   1258,  "windows-1258",     "Vietnamese (Windows)" ],
+
+  [ 46,   65001, "UTF-8",            "Unicode (UTF-8)" ],
+  [ 47,   65000, "UTF-7",            "Unicode (UTF-7)" ],
+
+  [ 48,   1200, "UTF-16",            "Unicode (UTF-16)" ],
+  [ 49,   1201, "UTF-16BE",          "Unicode (UTF-16 Big Endian)" ],
+
+  [ 50,   12000, "UTF-32",           "Unicode (UTF-32)" ],
+  [ 51,   12001, "UTF-32BE",         "Unicode (UTF-32 Big Endian)" ]
+];
+var c_oAscEncodingsMap = {"437": 43, "720": 1, "737": 21, "775": 5, "850": 39, "852": 15, "855": 12, "857": 35, "858": 40, "860": 41, "861": 30, "862": 25, "863": 42, "865": 31, "866": 13, "869": 22, "874": 32, "932": 27, "936": 18, "949": 28, "950": 17, "1200": 48, "1201": 49, "1250": 16, "1251": 14, "1252": 44, "1253": 23, "1254": 36, "1255": 26, "1256": 2, "1257": 6, "1258": 45, "10007": 11, "12000": 50, "12001": 51, "20866": 9, "21866": 10, "28591": 37, "28592": 19, "28593": 33, "28594": 3, "28595": 8, "28596": 0, "28597": 20, "28598": 24, "28599": 34, "28603": 4, "28604": 7, "28605": 38, "51949": 29, "65000": 47, "65001": 46}
+var c_oAscCodePageUtf8 = 46;//65001
+
 exports.TaskQueueData = TaskQueueData;
 exports.CMailMergeSendData = CMailMergeSendData;
 exports.InputCommand = InputCommand;
 exports.ErrorWithResult = ErrorWithResult;
+exports.c_oAscCsvDelimiter = c_oAscCsvDelimiter;
+exports.c_oAscEncodings = c_oAscEncodings;
+exports.c_oAscEncodingsMap = c_oAscEncodingsMap;
+exports.c_oAscCodePageUtf8 = c_oAscCodePageUtf8;
