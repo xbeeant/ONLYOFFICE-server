cbdccee1c754d185f9f52ef51e87edc5f460d42d
Alexander.Trofimov <Alexander.Trofimov@onlyoffice.com>
2016-03-09 10:44:41 +0000

    Merged revision(s) 68728-68732 from AVS/Sources/TeamlabOffice/branches/TeamlabOffice_v3.7_Fix_Old_Licenses:
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/nodeJSProjects@68733 954022d7-b5bf-4e40-9824-e11837661b57

--- Common/sources/license.js
+++ Common/sources/license.js
@@ -16,7 +16,7 @@ exports.readLicense = function() {
     const publicKey = '-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDRhGF7X4A0ZVlEg594WmODVVUI\niiPQs04aLmvfg8SborHss5gQXu0aIdUT6nb5rTh5hD2yfpF2WIW6M8z0WxRhwicg\nXwi80H1aLPf6lEPPLvN29EhQNjBpkFkAJUbS8uuhJEeKw0cE49g80eBBF4BCqSL6\nPFQbP9/rByxdxEoAIQIDAQAB\n-----END PUBLIC KEY-----\n';
     if (verify.verify(publicKey, sign, 'hex')) {
       const endDate = new Date(oLicense['end_date']);
-      if (endDate >= new Date()) {
+      if (endDate >= new Date() && 2 <= oLicense['version']) {
         res.count = Math.min(Math.max(res.count, oLicense['process'] >> 0), resMax.count);
         res.type = constants.LICENSE_RESULT.Success;
       } else {
