76e0f075b8c83dccc5e62ae547d4d3d2278d549d
Alexander.Trofimov <Alexander.Trofimov@onlyoffice.com>
2016-04-04 10:28:37 +0300

    win32 -> win_32

--- run.bat
+++ run.bat
@@ -23,7 +23,7 @@ copy "..\..\..\core\build\lib\win_32\PdfWriter.dll" "."
 copy "..\..\..\core\build\lib\win_32\HtmlFile.dll" "."
 copy "..\..\..\core\build\lib\win_32\UnicodeConverter.dll" "."
 copy "..\..\..\core\build\lib\win_32\HtmlFileInternal.exe" ".\HtmlFileInternal"
-xcopy /s/h/e/k/c/y/q "..\..\..\core\build\cef\win32" ".\HtmlFileInternal"
+xcopy /s/h/e/k/c/y/q "..\..\..\core\build\cef\win_32" ".\HtmlFileInternal"
 copy "..\..\..\core\build\bin\windows\x2t32.exe" "."
 
 "..\..\..\core\build\bin\AllFontsGen\windows_32.exe" "%windir%\Fonts" "%~dp0\..\sdkjs\common\AllFonts.js" "%~dp0\..\sdkjs\common\Images" "%~dp0\FileConverter\Bin\font_selection.bin"
