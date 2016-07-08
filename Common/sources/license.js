9e4f82988b1b0ce1b7b25e8cf5913285a4bf6343
Alexander.Trofimov <Alexander.Trofimov@onlyoffice.com>
2016-07-08 18:33:57 +0300

    check trial

--- Common/sources/license.js
+++ Common/sources/license.js
@@ -64,7 +64,8 @@ exports.readLicense = function*() {
 		const publicKey = '-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDRhGF7X4A0ZVlEg594WmODVVUI\niiPQs04aLmvfg8SborHss5gQXu0aIdUT6nb5rTh5hD2yfpF2WIW6M8z0WxRhwicg\nXwi80H1aLPf6lEPPLvN29EhQNjBpkFkAJUbS8uuhJEeKw0cE49g80eBBF4BCqSL6\nPFQbP9/rByxdxEoAIQIDAQAB\n-----END PUBLIC KEY-----\n';
 		if (verify.verify(publicKey, sign, 'hex')) {
 			const endDate = new Date(oLicense['end_date']);
-			if (endDate >= oBuildDate && 2 <= oLicense['version']) {
+			const checkDate = (false === oLicense['trial'] || 'false' === oLicense['trial']) ? oBuildDate : new Date(); // Someone who likes to put json string instead of bool
+			if (endDate >= checkDate && 2 <= oLicense['version']) {
 				res.count = Math.min(Math.max(res.count, oLicense['process'] >> 0), resMax.count);
 				res.type = constants.LICENSE_RESULT.Success;
 			} else {
