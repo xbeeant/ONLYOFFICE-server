09251d6f8823538139b4365d43e53c92a4af4dcb
konovalovsergey <Sergey.Konovalov@onlyoffice.com>
2017-03-06 18:14:08 +0300

    checkClientIp removed from '/plugins.json' query

--- DocService/sources/server.js
+++ DocService/sources/server.js
@@ -231,7 +231,7 @@ if (cluster.isMaster) {
 			res.setHeader('Content-Type', 'application/json');
 			res.send(JSON.stringify(data));
 		};
-		app.get('/plugins.json', utils.checkClientIp, (req, res) => {
+		app.get('/plugins.json', (req, res) => {
 			if (userPlugins && !updatePlugins) {
 				sendUserPlugins(res, userPlugins);
 				return;
