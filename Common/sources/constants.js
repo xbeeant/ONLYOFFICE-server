566ffd3fc02cb8463256805db42087b5f37d45af
Sergey Konovalov <Sergey.Konovalov@onlyoffice.com>
2020-07-31 13:45:46 +0300

    [bug] Fix bug 45976.
    Fix Path Traversal vulnerability via converterservice param - filetype. Add filetype allowlist via regex

--- Common/sources/constants.js
+++ Common/sources/constants.js
@@ -33,6 +33,8 @@
 'use strict';
 
 exports.DOC_ID_PATTERN = '0-9-.a-zA-Z_=';
+exports.DOC_ID_REGEX = new RegExp("^[" + exports.DOC_ID_PATTERN + "]*$", 'i');
+exports.EXTENTION_REGEX = /^[a-zA-Z0-9]*$/;
 exports.CHAR_DELIMITER = String.fromCharCode(5);
 exports.OUTPUT_NAME = 'output';
 exports.ONLY_OFFICE_URL_PARAM = 'ooname';
