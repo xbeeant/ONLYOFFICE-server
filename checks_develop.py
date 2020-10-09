02b76527d4af8dd28bd136118b5f8701bb6ef490
Nikita Khromov <Nikita.Khromov@onlyoffice.com>
2020-10-09 14:30:37 +0300

    Fix

--- checks_develop.py
+++ checks_develop.py
@@ -11,7 +11,7 @@ else:
   import _winreg as winreg
     
 def get_mysqlServersInfo():
-  dictInfo = {'Location': [], 'Version': [], 'DataLocation' : []}
+  arrInfo = []
   
   aReg = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
   aKey= winreg.OpenKey(aReg, "SOFTWARE\\", 0, winreg.KEY_READ | winreg.KEY_WOW64_32KEY)
@@ -24,51 +24,49 @@ def get_mysqlServersInfo():
       MySQLsubkey_name = winreg.EnumKey(asubkey, i)
       if (MySQLsubkey_name.find('MySQL Server') != - 1):
         MySQLsubkey = winreg.OpenKey(asubkey, MySQLsubkey_name)
-        dictInfo['Location'].append(winreg.QueryValueEx(MySQLsubkey, 'Location')[0])
-        dictInfo['Version'].append(winreg.QueryValueEx(MySQLsubkey, 'Version')[0])
-        dictInfo['DataLocation'].append(winreg.QueryValueEx(MySQLsubkey, 'DataLocation')[0])
+        dictInfo = {}
+        dictInfo['Location']  = winreg.QueryValueEx(MySQLsubkey, 'Location')[0]
+        dictInfo['Version'] = winreg.QueryValueEx(MySQLsubkey, 'Version')[0]
+        dictInfo['DataLocation'] = winreg.QueryValueEx(MySQLsubkey, 'DataLocation')[0]
+        arrInfo.append(dictInfo)
   except:
     pass
       
-  return dictInfo
+  return arrInfo
 
 def check_mysqlServer():
   base.print_info('Check MySQL Server')
   
   dependence = _dependence.CDependencies()
+  arrInfo = get_mysqlServersInfo()
   
-  dictInfo = get_mysqlServersInfo()
-  mySQLServersPaths = dictInfo['Location']
-  mySQLServersVersions = dictInfo['Version']
-  mySQLServersDataPaths = dictInfo['DataLocation']
-  
-  for i in range(len(mySQLServersPaths)):
-    if (base.is_dir(mySQLServersPaths[i]) == False):
+  for info in arrInfo:
+    if (base.is_dir(info['Location']) == False):
       continue
       
-    version_info = base.run_command('"' + mySQLServersPaths[i] + 'bin\\mysql" --version')['stdout']
+    version_info = base.run_command('"' + info['Location'] + 'bin\\mysql" --version')['stdout']
     if (version_info.find('for Win64') != -1):
-      print('MySQL Server ' + mySQLServersVersions[i] + ' bitness is valid')
-      connectionResult = base.run_command('"' + mySQLServersPaths[i] + 'bin\\mysql" -u root -ponlyoffice -e "SHOW GLOBAL VARIABLES LIKE ' + r"'PORT';" + '"')['stdout']
+      print('MySQL Server ' + info['Version'] + ' bitness is valid')
+      connectionResult = base.run_command('"' + info['Location'] + 'bin\\mysql" -u root -ponlyoffice -e "SHOW GLOBAL VARIABLES LIKE ' + r"'PORT';" + '"')['stdout']
       if (connectionResult.find('port') != -1 and connectionResult.find('3306') != -1):
-        print('MySQL Server ' + mySQLServersVersions[i] + ' configuration is valid')
-        if (base.run_command('"' + mySQLServersPaths[i] + 'bin\\mysql" -u root -ponlyoffice -e "SHOW DATABASES;"')['stdout'].find('onlyoffice') == -1):
+        print('MySQL Server ' + info['Version'] + ' configuration is valid')
+        if (base.run_command('"' + info['Location'] + 'bin\\mysql" -u root -ponlyoffice -e "SHOW DATABASES;"')['stdout'].find('onlyoffice') == -1):
           print('Database onlyoffice not found')
           dependence.append_install('MySQLDatabase')
-        if (base.run_command('"' + mySQLServersPaths[i] + 'bin\\mysql" -u root -ponlyoffice -e "SELECT plugin from mysql.user where User=' + "'root';")['stdout'].find('mysql_native_password') == -1):
+        if (base.run_command('"' + info['Location'] + 'bin\\mysql" -u root -ponlyoffice -e "SELECT plugin from mysql.user where User=' + "'root';")['stdout'].find('mysql_native_password') == -1):
           print('Password encryption is not valid')
           dependence.append_install('MySQLEncrypt') 
-        dependence.pathToValidMySQLServer = mySQLServersPaths[i]
+        dependence.pathToValidMySQLServer = info['Location']
         return dependence
-      print('MySQL Server ' + mySQLServersVersions[i] + ' configuration is not valid')
+      print('MySQL Server ' + info['Version'] + ' configuration is not valid')
     else: 
-      print('MySQL Server ' + mySQLServersVersions[i] + ' bitness is not valid')
+      print('MySQL Server ' + info['Version'] + ' bitness is not valid')
       
   print('Valid MySQL Server not found')
   
-  for i in range(len(mySQLServersVersions)):
-    dependence.append_uninstall('MySQL Server ' + mySQLServersVersions[i][0:3])
-    dependence.append_removepath(mySQLServersDataPaths[i])
+  for info in arrInfo:
+    dependence.append_uninstall('MySQL Server')
+    dependence.append_removepath(info['DataLocation'])
     
   dependence.append_install('MySQLServer')
   
