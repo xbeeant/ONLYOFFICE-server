4237a69fe08aec639f99e8e6272484ea7b378040
Alexander Trofimov <Alexander.Trofimov@onlyoffice.com>
2020-10-12 11:23:56 +0300

    [check] Fix
    Use check from base

--- checks_develop.py
+++ checks_develop.py
@@ -2,73 +2,7 @@ import sys
 sys.path.append('../build_tools/scripts')
 import base
 import dependence as _dependence
-import shutil
-import os
 import subprocess
-
-if (sys.version_info[0] >= 3):
-  import winreg
-else:
-  import _winreg as winreg
-    
-def get_mysqlServersInfo():
-  arrInfo = []
-  
-  try:
-    aReg = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
-    aKey = winreg.OpenKey(aReg, "SOFTWARE\\", 0, winreg.KEY_READ | winreg.KEY_WOW64_32KEY)
-  
-    asubkey = winreg.OpenKey(aKey, 'MySQL AB')
-    count_subkey = winreg.QueryInfoKey(asubkey)[0]
-    
-    for i in range(count_subkey):
-      MySQLsubkey_name = winreg.EnumKey(asubkey, i)
-      if (MySQLsubkey_name.find('MySQL Server') != - 1):
-        MySQLsubkey = winreg.OpenKey(asubkey, MySQLsubkey_name)
-        dictInfo = {}
-        dictInfo['Location']  = winreg.QueryValueEx(MySQLsubkey, 'Location')[0]
-        dictInfo['Version'] = winreg.QueryValueEx(MySQLsubkey, 'Version')[0]
-        dictInfo['DataLocation'] = winreg.QueryValueEx(MySQLsubkey, 'DataLocation')[0]
-        arrInfo.append(dictInfo)
-  except:
-    pass
-      
-  return arrInfo
-
-def check_mysqlServer():
-  base.print_info('Check MySQL Server')
-  
-  dependence = _dependence.CDependencies()
-  arrInfo = get_mysqlServersInfo()
-  
-  for info in arrInfo:
-    if (base.is_dir(info['Location']) == False):
-      continue
-      
-    version_info = base.run_command('"' + info['Location'] + 'bin\\mysql" --version')['stdout']
-    if (version_info.find('for Win64') != -1):
-      print('MySQL Server ' + info['Version'] + ' bitness is valid')
-      connectionResult = base.run_command('"' + info['Location'] + 'bin\\mysql" -u root -ponlyoffice -e "SHOW GLOBAL VARIABLES LIKE ' + r"'PORT';" + '"')['stdout']
-      if (connectionResult.find('port') != -1 and connectionResult.find('3306') != -1):
-        print('MySQL Server ' + info['Version'] + ' configuration is valid')
-        dependence.mysqlPath = info['Location']
-        return dependence
-      print('MySQL Server ' + info['Version'] + ' configuration is not valid')
-    else:
-      print('MySQL Server ' + info['Version'] + ' bitness is not valid')
-      
-  print('Valid MySQL Server not found')
-  
-  dependence.append_uninstall('MySQL Server')
-  dependence.append_install('MySQLServer')
-  
-  MySQLData = os.environ['ProgramData'] + '\\MySQL\\'
-  dir = os.listdir(MySQLData)
-  for path in dir:
-    if (path.find('MySQL Server') != -1) and (base.is_file(MySQLData + path) == False):
-      dependence.append_removepath(MySQLData + path)
-  
-  return dependence
 
 def check_MySQLConfig(mysqlPath = ''):
   dependence = _dependence.CDependencies()
@@ -122,7 +56,7 @@ def check_dependencies():
   final_dependence.append(_dependence.check_gruntcli())
   final_dependence.append(_dependence.check_buildTools())
   final_dependence.append(_dependence.check_mysqlInstaller())
-  final_dependence.append(check_mysqlServer())
+  final_dependence.append(_dependence.check_mysqlServer())
   
   return final_dependence
   
