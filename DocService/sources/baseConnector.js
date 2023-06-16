6728395f1e80e973ccef99b5faf0b3dd1bd21aab
Georgii Petrov <gpetrov-job@yandex.ru>
2023-06-16 16:25:59 +0300

    [ds] Oracle base connector pt.1

--- DocService/sources/baseConnector.js
+++ DocService/sources/baseConnector.js
@@ -36,15 +36,18 @@ var sqlDataBaseType = {
 	mySql		: 'mysql',
 	mariaDB		: 'mariadb',
 	postgreSql	: 'postgres',
-	dameng	: 'dameng'
+	dameng	: 'dameng',
+    oracle: 'oracle'
 };
 
+const connectorUtilities = require('./connectorUtilities');
 var bottleneck = require("bottleneck");
 var config = require('config');
 var configSql = config.get('services.CoAuthoring.sql');
+const dbType = configSql.get('type');
 
 var baseConnector;
-switch (configSql.get('type')) {
+switch (dbType) {
   case sqlDataBaseType.mySql:
   case sqlDataBaseType.mariaDB:
     baseConnector = require('./mySqlBaseConnector');
@@ -52,11 +55,13 @@ switch (configSql.get('type')) {
   case sqlDataBaseType.dameng:
     baseConnector = require('./damengBaseConnector');
     break;
+  case sqlDataBaseType.oracle:
+    baseConnector = require('./oracleBaseConnector');
+    break;
   default:
     baseConnector = require('./postgreSqlBaseConnector');
     break;
 }
-let constants = require('./../../Common/sources/constants');
 
 const cfgTableResult = configSql.get('tableResult');
 const cfgTableChanges = configSql.get('tableChanges');
@@ -275,7 +280,18 @@ exports.healthCheck = function (ctx) {
   return new Promise(function(resolve, reject) {
   	//SELECT 1; usefull for H2, MySQL, Microsoft SQL Server, PostgreSQL, SQLite
   	//http://stackoverflow.com/questions/3668506/efficient-sql-test-query-or-validation-query-that-will-work-across-all-or-most
-    baseConnector.sqlQuery(ctx, 'SELECT 1;', function(error, result) {
+    let sql;
+    switch (dbType) {
+      case sqlDataBaseType.oracle: {
+        sql = 'SELECT 1 FROM DUAL;';
+        break;
+      }
+      default: {
+        sql = 'SELECT 1;';
+      }
+    }
+
+    baseConnector.sqlQuery(ctx, sql, function(error, result) {
       if (error) {
         reject(error);
       } else {
@@ -313,156 +329,6 @@ exports.getTableColumns = function(ctx, tableName) {
     });
   }
 };
-function UserCallback() {
-  this.userIndex = undefined;
-  this.callback = undefined;
-}
-UserCallback.prototype.fromValues = function(userIndex, callback){
-  if(null !== userIndex){
-    this.userIndex = userIndex;
-  }
-  if(null !== callback){
-    this.callback = callback;
-  }
-};
-UserCallback.prototype.delimiter = constants.CHAR_DELIMITER;
-UserCallback.prototype.toSQLInsert = function(){
-  return this.delimiter + JSON.stringify(this);
-};
-UserCallback.prototype.getCallbackByUserIndex = function(ctx, callbacksStr, opt_userIndex) {
-  ctx.logger.debug("getCallbackByUserIndex: userIndex = %s callbacks = %s", opt_userIndex, callbacksStr);
-  if (!callbacksStr || !callbacksStr.startsWith(UserCallback.prototype.delimiter)) {
-    let index = callbacksStr.indexOf(UserCallback.prototype.delimiter);
-    if (-1 === index) {
-      //old format
-      return callbacksStr;
-    } else {
-      //mix of old and new format
-      callbacksStr = callbacksStr.substring(index);
-    }
-  }
-  let callbacks = callbacksStr.split(UserCallback.prototype.delimiter);
-  let callbackUrl = "";
-  for (let i = 1; i < callbacks.length; ++i) {
-    let callback = JSON.parse(callbacks[i]);
-    callbackUrl = callback.callback;
-    if (callback.userIndex === opt_userIndex) {
-      break;
-    }
-  }
-  return callbackUrl;
-};
-UserCallback.prototype.getCallbacks = function(ctx, callbacksStr) {
-  ctx.logger.debug("getCallbacks: callbacks = %s", callbacksStr);
-  if (!callbacksStr || !callbacksStr.startsWith(UserCallback.prototype.delimiter)) {
-    let index = callbacksStr.indexOf(UserCallback.prototype.delimiter);
-    if (-1 === index) {
-      //old format
-      return [callbacksStr];
-    } else {
-      //mix of old and new format
-      callbacksStr = callbacksStr.substring(index);
-    }
-  }
-  let callbacks = callbacksStr.split(UserCallback.prototype.delimiter);
-  let res = [];
-  for (let i = 1; i < callbacks.length; ++i) {
-    let callback = JSON.parse(callbacks[i]);
-    res.push(callback.callback);
-  }
-  return res;
-};
-exports.UserCallback = UserCallback;
-
-function DocumentPassword() {
-  this.password = undefined;
-  this.change = undefined;
-}
-DocumentPassword.prototype.fromString = function(passwordStr){
-  var parsed = JSON.parse(passwordStr);
-  this.fromValues(parsed.password, parsed.change);
-};
-DocumentPassword.prototype.fromValues = function(password, change){
-  if(null !== password){
-    this.password = password;
-  }
-  if(null !== change) {
-    this.change = change;
-  }
-};
-DocumentPassword.prototype.delimiter = constants.CHAR_DELIMITER;
-DocumentPassword.prototype.toSQLInsert = function(){
-  return this.delimiter + JSON.stringify(this);
-};
-DocumentPassword.prototype.isInitial = function(){
-  return !this.change;
-};
-DocumentPassword.prototype.getDocPassword = function(ctx, docPasswordStr) {
-  let res = {initial: undefined, current: undefined, change: undefined};
-  if (docPasswordStr) {
-    ctx.logger.debug("getDocPassword: passwords = %s", docPasswordStr);
-    let passwords = docPasswordStr.split(UserCallback.prototype.delimiter);
-
-    for (let i = 1; i < passwords.length; ++i) {
-      let password = new DocumentPassword();
-      password.fromString(passwords[i]);
-      if (password.isInitial()) {
-        res.initial = password.password;
-      } else {
-        res.change = password.change;
-      }
-      res.current = password.password;
-    }
-  }
-  return res;
-};
-DocumentPassword.prototype.getCurPassword = function(ctx, docPasswordStr) {
-  let docPassword = this.getDocPassword(ctx, docPasswordStr);
-  return docPassword.current;
-};
-DocumentPassword.prototype.hasPasswordChanges = function(ctx, docPasswordStr) {
-  let docPassword = this.getDocPassword(ctx, docPasswordStr);
-  return docPassword.initial !== docPassword.current;
-};
-exports.DocumentPassword = DocumentPassword;
-
-function DocumentAdditional() {
-  this.data = [];
-}
-DocumentAdditional.prototype.delimiter = constants.CHAR_DELIMITER;
-DocumentAdditional.prototype.toSQLInsert = function() {
-  if (this.data.length) {
-    let vals = this.data.map((currentValue) => {
-      return JSON.stringify(currentValue);
-    });
-    return this.delimiter + vals.join(this.delimiter);
-  } else {
-    return null;
-  }
-};
-DocumentAdditional.prototype.fromString = function(str) {
-  if (!str) {
-    return;
-  }
-  let vals = str.split(this.delimiter).slice(1);
-  this.data = vals.map((currentValue) => {
-    return JSON.parse(currentValue);
-  });
-};
-DocumentAdditional.prototype.setOpenedAt = function(time, timezoneOffset) {
-  let additional = new DocumentAdditional();
-  additional.data.push({time: time, timezoneOffset: timezoneOffset});
-  return additional.toSQLInsert();
-};
-DocumentAdditional.prototype.getOpenedAt = function(str) {
-  let res;
-  let val = new DocumentAdditional();
-  val.fromString(str);
-  val.data.forEach((elem) => {
-    if (undefined !== elem.timezoneOffset) {
-      res = elem.time - (elem.timezoneOffset * 60 * 1000);
-    }
-  });
-  return res;
-};
-exports.DocumentAdditional = DocumentAdditional;
+exports.UserCallback = connectorUtilities.UserCallback;
+exports.DocumentPassword = connectorUtilities.DocumentPassword;
+exports.DocumentAdditional = connectorUtilities.DocumentAdditional;
---
+++ DocService/sources/oracleBaseConnector.js
@@ -0,0 +1,174 @@
+/*
+ * (c) Copyright Ascensio System SIA 2010-2023
+ *
+ * This program is a free software product. You can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License (AGPL)
+ * version 3 as published by the Free Software Foundation. In accordance with
+ * Section 7(a) of the GNU AGPL its Section 15 shall be amended to the effect
+ * that Ascensio System SIA expressly excludes the warranty of non-infringement
+ * of any third-party rights.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For
+ * details, see the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
+ *
+ * You can contact Ascensio System SIA at 20A-6 Ernesta Birznieka-Upish
+ * street, Riga, Latvia, EU, LV-1050.
+ *
+ * The  interactive user interfaces in modified source and object code versions
+ * of the Program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU AGPL version 3.
+ *
+ * Pursuant to Section 7(b) of the License you must retain the original Product
+ * logo when distributing the program. Pursuant to Section 7(e) we decline to
+ * grant you any rights under trademark law for use of our trademarks.
+ *
+ * All the Product's GUI elements, including illustrations and icon sets, as
+ * well as technical writing content are licensed under the terms of the
+ * Creative Commons Attribution-ShareAlike 4.0 International. See the License
+ * terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
+ *
+ */
+
+'use strict';
+
+const oracledb = require('oracledb');
+const config = require('config');
+const connectorUtilities = require("./connectorUtilities");
+
+const configSql = config.get('services.CoAuthoring.sql');
+const cfgTableResult = config.get('services.CoAuthoring.sql.tableResult');
+
+const connectionConfiguration = {
+  user: 'pdbadmin', // configSql.get('dbUser')
+  password: 'admin', // configSql.get('dbPass')
+  connectString: '192.168.5.7:1521/xepdb1', // `${configSql.get('dbHost')}:${configSql.get('dbPort')}/${configSql.get('dbName')}`
+  poolMin: 0,
+  poolMax: 10  // configSql.get('connectionlimit')
+};
+let pool = null;
+
+async function sqlQuery(ctx, sqlCommand, callbackFunction, opt_noModifyRes, opt_noLog, opt_values) {
+  try {
+    if (!pool) {
+      pool = await oracledb.createPool(connectionConfiguration);
+    }
+
+    const connection = await oracledb.getConnection();
+
+    const handler = (error, result) => {
+      if (error) {
+        if (!opt_noLog) {
+          ctx.logger.error('sqlQuery error sqlCommand: %s: %s', sqlCommand.slice(0, 50), error.stack);
+        }
+
+        callbackFunction?.(error);
+
+        return;
+      }
+
+      let output = result;
+      if (!opt_noModifyRes) {
+        output = { rows: [], affectedRows: 0 };
+
+        if (result?.rowsAffected) {
+          output = { affectedRows: result.rowsAffected };
+        }
+
+        if (result?.rows) {
+          output = { rows: result.rows };
+        }
+      }
+
+      connection.close();
+      callbackFunction?.(error, output);
+    };
+
+    if (opt_values) {
+      connection.execute(sqlCommand, opt_values, handler);
+    } else {
+      connection.execute(sqlCommand, handler);
+    }
+  } catch (error) {
+    if (!opt_noLog) {
+      ctx.logger.error('sqlQuery error while pool manipulation: %s', error.stack);
+    }
+
+    callbackFunction?.(error);
+  }
+}
+
+function addSqlParameter(parameter, accumulatedArray) {
+  const currentIndex = accumulatedArray.push(parameter) - 1;
+  return `:${currentIndex}`;
+}
+
+function concatParams(val1, val2) {
+  return `COALESCE(${val1}, '') || COALESCE(${val2}, '')`;
+}
+
+function upsert(ctx, task, opt_updateUserIndex) {
+  return new Promise((resolve, reject) => {
+    task.completeDefaults();
+
+    let cbInsert = task.callback;
+    if (task.callback) {
+      const userCallback = new connectorUtilities.UserCallback();
+      userCallback.fromValues(task.userIndex, task.callback);
+      cbInsert = userCallback.toSQLInsert();
+    }
+
+    const dateNow = new Date();
+    const values = [];
+    const valuesPlaceholder = [
+      addSqlParameter(task.tenant, values),
+      addSqlParameter(task.key, values),
+      addSqlParameter(task.status, values),
+      addSqlParameter(task.statusInfo, values),
+      addSqlParameter(dateNow, values),
+      addSqlParameter(task.userIndex, values),
+      addSqlParameter(task.changeId, values),
+      addSqlParameter(cbInsert, values),
+      addSqlParameter(task.baseurl, values)
+    ];
+
+    let callback = '';
+    if (task.callback) {
+      const parameter = addSqlParameter(JSON.stringify(task.callback), values);
+      callback = `, callback = CONCAT(callback , '${connectorUtilities.UserCallback.prototype.delimiter}{"userIndex":' , (user_index + 1) , ',"callback":', ${parameter}, '}')`;
+    }
+
+    let baseUrl = '';
+    if (task.baseurl) {
+      const parameter = addSqlParameter(task.baseurl, values);
+      baseUrl = `, baseurl = ${parameter}`;
+    }
+
+    let userIndex = '';
+    if (opt_updateUserIndex) {
+      userIndex = ', user_index = user_index + 1';
+    }
+
+    const updateQuery = `last_open_date = ${addSqlParameter(dateNow, values)}${callback}${baseUrl}${userIndex}`
+    const condition = `tenant = ${valuesPlaceholder[0]} AND id = ${valuesPlaceholder[1]}`
+
+    let mergeSqlCommand = `MERGE INTO ${cfgTableResult} USING DUAL searching_alias ON (${condition})`
+      + ` WHEN MATCHED THEN UPDATE SET ${updateQuery}`
+      + ` WHEN NOT MATCHED THEN INSERT (tenant, id, status, status_info, last_open_date, user_index, change_id, callback, baseurl) VALUES (${valuesPlaceholder.join(', ')});`;
+
+    exports.sqlQuery(ctx, mergeSqlCommand, function(error, result) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(result);
+      }
+    }, undefined, undefined, values);
+  });
+}
+
+module.exports = {
+  sqlQuery,
+  addSqlParameter,
+  concatParams,
+  upsert
+}
